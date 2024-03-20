7435f28b9d29e03b99c97bc0f9b6aa9852a246c0
Leire Lasa <leire.lasa.amo@gmail.com>
2024-03-20 11:04:25 +0100

    added MYA website's menu

---
+++ language-selector-MYA/js/language-selector.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const locales = ["en-GB", "cs-CZ", "es-ES", "fr-FR", "ru-RU", "hi-IN", "zh-CN"];
+    const localeToUrl = {
+        'en': 'https://www.myaudit.org/',
+        'cs': 'https://www.myaudit.org/cz/',
+        'es': 'https://www.myaudit.org/es/',
+        'fr': 'https://www.myaudit.org/fr/',
+        'ru': 'https://www.myaudit.org/ru/',
+        'hi': 'https://www.myaudit.org/in/',
+        'zh': 'https://www.myaudit.org/cn/'
+    };
+
+    function getFlagSrc(countryCode) {
+        return /^[A-Z]{2}$/.test(countryCode)
+            ? `https://flagsapi.com/${countryCode.toUpperCase()}/shiny/64.png`
+            : "";
+    }
+
+    const dropdownContent = document.getElementById('dropdown-content');
+
+	locales.forEach((locale) => {
+		const lang = new Intl.Locale(locale);
+		const flagSrc = getFlagSrc(lang.region);
+		const link = document.createElement("a");
+		link.className = "flag-link";
+		link.href = localeToUrl[lang.language];
+		link.innerHTML = `<img src="${flagSrc}" alt="${lang.language} flag" style="width: 25px; text-align: center;">`;
+		dropdownContent.appendChild(link);
+	});
+
+    document.querySelector(".dropbtn").addEventListener("click", function () {
+        const dropdownContent = document.querySelector(".dropdown-content");
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+});
