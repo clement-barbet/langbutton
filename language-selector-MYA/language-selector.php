d1cc5f80ad8ccb70c6ce3c09440dd4a8503919cc
Leire Lasa <leire.lasa.amo@gmail.com>
2024-04-15 17:20:09 +0200

    MYA: Selected language showing in button and auto-close when click outside

--- language-selector-MYA/language-selector.php
+++ language-selector-MYA/language-selector.php
@@ -4,7 +4,7 @@
  * Plugin name: Language Selector Menu
  * Description: A simple plugin to add a language selector menu to the website.
  * Author: Leire Lasa
- * Version: 1.2
+ * Version: 2.0
  * Text Domain: language-selector-menu
  */
 
